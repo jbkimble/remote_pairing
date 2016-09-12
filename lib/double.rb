a78065881e3bb7c693cb8753706bd7612adab3c2
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-09-12 10:05:28 -0600

    did some stuff

--- lib/double.rb
+++ lib/double.rb
@@ -1,8 +1,23 @@
-class Double
-  attr_accessor :input_a, :input_b
-  def initialize
-    @input_a = 0
-    @input_b = 0
+module And
+
+  def output
+    out = self.instance_variables.map { |v| v.to_s.delete("@") }
+    result = out.reduce([]) do |result, input|
+      result << self.send(input)
+      result
+    end
+    return 0 if result.include?(0)
+    return 1
   end
 
+  class Double
+    include And
+    attr_accessor :input_a, :input_b
+
+    def initialize
+      @input_a = 0
+      @input_b = 0
+    end
+
+  end
 end
--- test/double_test.rb
+++ test/double_test.rb
@@ -5,18 +5,34 @@ require './lib/double'
 
 class DoubleTest < Minitest::Test
   def test_variables_are_initialized_as_zero
-    tr = Double.new
+    tr = And::Double.new
 
     assert_equal 0, tr.input_a
     assert_equal 0, tr.input_b
   end
 
   def test_variables_can_be_changed
-    tr = Double.new
+    tr = And::Double.new
     tr.input_a = 1
     tr.input_b = 1
 
     assert_equal 1, tr.input_a
     assert_equal 1, tr.input_b
   end
+
+  def test_we_can_do_this_and_shit
+    tr = And::Double.new
+    assert tr.output
+  end
+
+  def test_that_we_can_effect_output
+    tr = And::Double.new
+    tr.input_a = 1
+    tr.input_b = 1
+
+    assert_equal 1, tr.output
+  end
+
+
+
 end
